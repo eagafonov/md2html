2cf645be0951f00ee1550599ccb03585bacb26b9
Eugene Agafonov <e.a.agafonov@gmail.com>
2017-02-16 13:45:19 +0300

    Fix watcher

--- md2html_watch.sh
+++ md2html_watch.sh
@@ -8,7 +8,7 @@ INPUT_FILENAME=$1
 test -f $INPUT_FILENAME
 
 while (true); do 
-    inotifywait --event close_nowrite $INPUT_FILENAME || true;
+    inotifywait --event close_write $INPUT_FILENAME || true;
     md2html.sh $INPUT_FILENAME
 done
     
