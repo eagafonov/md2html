2b364315561ff5422faed2acd1f8501ee5cb2424
Eugene Agafonov <e.a.agafonov@gmail.com>
2017-02-16 13:44:58 +0300

    Specify target basename as <!---md2html:basename--->

--- md2html.sh
+++ md2html.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 set -e
 
-
 INPUT_FILENAME=$1
 
+BASE_NAME=$(grep '<!---md2html:' $INPUT_FILENAME | head -1|sed 's/^<!---md2html:\(.*\)--->/\1/')
+
+if [ -z "$BASE_NAME" ]; then
+    BASE_NAME=${INPUT_FILENAME%.md}
+fi
+
 # replace .md with .html
-OUTPUT_FILENAME=${INPUT_FILENAME%.md}.html
+OUTPUT_FILENAME=${BASE_NAME}.html
+
+echo "OUTPUT_FILENAME=$OUTPUT_FILENAME"
 
 (
 echo '<!DOCTYPE html>
