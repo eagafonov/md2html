0603d0314e4020cf270dc68a3f1955392dccb7cf
Eugene Agafonov <e.a.agafonov@gmail.com>
2018-03-02 14:47:04 +0300

    Watch style sheet for changes

--- md2html_watch.sh
+++ md2html_watch.sh
@@ -7,8 +7,14 @@ INPUT_FILENAME=$1
 
 test -f $INPUT_FILENAME
 
+WATCH_FILES=$INPUT_FILENAME
+
+if test -f "style.css"; then
+    WATCH_FILES=$WATCH_FILES style.css
+fi
+
 while (true); do 
-    inotifywait --event close_write $INPUT_FILENAME style.css || true;
+    inotifywait --event close_write $WATCH_FILES || true;
     md2html.sh $INPUT_FILENAME
 done
     
